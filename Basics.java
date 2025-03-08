9a16ad45d7f4742832789060e645c6a00c85f82d
abhinavsingh10 <105665656+abhinavsingh10@users.noreply.github.com>
2025-03-08 21:11:22 +0530

    first commit

---
+++ Basics.java
@@ -0,0 +1,6 @@
+class Basics {
+    public static void main(String[] args) {
+        int arr[][] = { { 1, 3, 23 }, { 2 }, { 9, 56, 88 }, { 4, 5 } };
+        System.out.println(arr[2][1]);
+    }
+}
