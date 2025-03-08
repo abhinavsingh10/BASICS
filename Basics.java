abe45f5a96ac50c71c2e9eb3df6e7168646346ef
abhinavsingh10 <105665656+abhinavsingh10@users.noreply.github.com>
2025-03-08 21:12:38 +0530

    My first java program

--- Basics.java
+++ Basics.java
@@ -1,6 +1,6 @@
 class Basics {
     public static void main(String[] args) {
-        int arr[][] = { { 1, 3, 23 }, { 2 }, { 9, 56, 88 }, { 4, 5 } };
+        int arr[][] = { { 1, 3, 23 }, { 2 }, { 9, 56, 88 }, { 4, 5 }, { 10, 25 } };
         System.out.println(arr[2][1]);
     }
 }
